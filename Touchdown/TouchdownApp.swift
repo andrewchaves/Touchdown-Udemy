d679f08b53558d0651c52526c9984c192bd2eb93
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-06-16 00:03:18 -0400

    Initial commit with project resources

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Andrew Vale on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
