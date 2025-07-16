9406a1eb2bd2eecb73a871cf224785d4cc1388a8
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-07-15 23:44:34 -0400

    create header and footer component

---
+++ Touchdown/View/Home/SectionView.swift
@@ -0,0 +1,18 @@
+//
+//  SectionView.swift
+//  Touchdown
+//
+//  Created by Andrew Vale on 15/07/25.
+//
+
+import SwiftUI
+
+struct SectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionView()
+}
