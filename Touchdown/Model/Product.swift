18e78a6da6e91cf22baaf5b6a5763c541eaaff2e
Andrew Chaves <andrew.almeidachaves@gmail.com>
2025-07-25 23:16:43 -0400

    Create Product Model

---
+++ Touchdown/Model/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  Touchdown
+//
+//  Created by Andrew Vale on 25/07/25.
+//
+
+import SwiftUI
+
+struct Product: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Product()
+}
